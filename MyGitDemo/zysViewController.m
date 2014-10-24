7e94bfc7ebca58ee0f7a36d2f6f2184c72a7867e
张远山 <zhangyuanshan@zhangyuanshanmatoMac-mini.local>
2014-10-24 16:29:19 +0800

    first submit

--- MyGitDemo/zysViewController.m
+++ MyGitDemo/zysViewController.m
@@ -9,7 +9,7 @@
 #import "zysViewController.h"
 
 @interface zysViewController ()
-
+@property (nonatomic) int sum;
 @end
 
 @implementation zysViewController
@@ -22,8 +22,12 @@
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
+  [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+  int a = 5;
+  int b = 10;
+  self.sum = a + b;
+  NSLog(@"The result is: %d", self.sum);
 }
 
 @end
