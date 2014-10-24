d4bbe7e06d8c0603fcba06ff053eb3f6e68a7ef9
张远山 <zhangyuanshan@zhangyuanshanmatoMac-mini.local>
2014-10-24 15:14:58 +0800

    Initial Commit

---
+++ MyGitDemo/zysViewController.m
@@ -0,0 +1,29 @@
+//
+//  zysViewController.m
+//  MyGitDemo
+//
+//  Created by 张远山 on 14-10-24.
+//  Copyright (c) 2014年 张远山. All rights reserved.
+//
+
+#import "zysViewController.h"
+
+@interface zysViewController ()
+
+@end
+
+@implementation zysViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
